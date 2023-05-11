1a466ab98b44b9442eacddcb32e097d99f39aa1d
Giny23 <vduquenoy23@gmail.com>
2023-05-11 17:43:36 +0200

    test

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,14 +13,15 @@ class HomeController extends AbstractController
     {
         return $this->twig->render('Home/index.html.twig');
     }
+
     public function sort($question1, $question2, $question3, $question4)
     {
         $question1 = $question2 = $question3 = $question4 = "";
         if ($_SERVER['REQUEST_METHOD'] === 'post') {
-            $question1 = $_POST['']
+            $question1 = $_POST[''];
             $itemManager = new ItemManager();
             $result = $itemManager->sort($question1, $question2, $question3, $question4);
             return $result;
-
+        }
     }
 }
