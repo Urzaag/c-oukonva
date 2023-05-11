c197b7b3a624a4794f136d346cab7199166875ad
Giny23 <vduquenoy23@gmail.com>
2023-05-11 23:06:51 +0200

    variables

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -18,13 +18,68 @@ class HomeController extends AbstractController
     {
         $question1 = $question2 = $question3 = $question4 = "";
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $question1 = $_POST['question1'];
-            $question2 = $_POST['question2'];
-            $question3 = $_POST['question3'];
-            $question4 = $_POST['question4'];
+
+            if (isset($_POST['question1'])){
+                $question1 = $_POST['question1'];
+            } else {
+                $question1 = 'jonhy';
+            }
+
+            if (isset($_POST['question2'])){
+                $question2 = $_POST['question2'];
+            } elseif ($question1 = 'jonhy'){
+                $question2 = 'slip';
+            } else {
+                $question2 = 'claquette';
+            }
+
+            if (isset($_POST['question3'])){
+                $question3 = $_POST['question3'];
+            } elseif ($question1 = 'jonhy') {
+                $question3 = 'mulet';
+            } else {
+                $question2 = 'claquette';
+                $question3 = 'bob';
+            }
+
+            if (isset($_POST['question4'])){
+                $question4 = $_POST['question4'];
+            } elseif ($question1 = 'jonhy') {
+                $question4 = 'ricard';
+            } else {
+                $question2 = 'claquette';
+                $question3 = 'bob';
+                $question4 = 'biere';
+            }
+
             $itemManager = new ItemManager();
             $result = $itemManager->sort($question1, $question2, $question3, $question4);
-            return $this->twig->render('Map/map.html.twig', ['result' => $result]);
+            if (empty($result)){
+                $result = ['id' => 5];
+            }
+
+            foreach ($result as $camping){
+                $campingID[] = $camping['id'];
+            }
+
+            $display1 = $display2 = $display3 = $display4 = $display5 = "";
+
+            if (in_array('1', $campingID)){
+                $display1 = 'display';
+            }
+            if (in_array('2', $campingID)){
+                $display2 = 'display';
+            }
+            if (in_array('3', $campingID)){
+                $display3 = 'display';
+            }
+            if (in_array('4', $campingID)){
+                $display4 = 'display';
+            }
+            if (in_array('5', $campingID)){
+                $display5 = 'display';
+            }
+            return $this->twig->render('Map/map.html.twig', ['result' => $result, 'display1' => $display1, 'display2' => $display2, 'display3' => $display3, 'display4' => $display4, 'display5' => $display5]);
         }
     }
 }
